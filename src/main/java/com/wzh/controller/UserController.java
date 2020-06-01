127a6445408f0cc2f9e166055e115d8d3ed15a04
WZH626 <60842317+WZH626@users.noreply.github.com>
2020-06-01 21:35:13 +0800

    Update UserController.java

--- src/main/java/com/wzh/controller/UserController.java
+++ src/main/java/com/wzh/controller/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("user")
 public class UserController {
     public static void main(String[] args) {
-        System.out.println("user");
+        System.out.println("user+name");
     }
 
 }
