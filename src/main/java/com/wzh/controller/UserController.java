d3504f805f60d033a8ad7960d5855001c1156cb9
WZH626 <1531909589@qq.com>
2020-06-01 21:23:31 +0800

    用户Controller

---
+++ src/main/java/com/wzh/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.wzh.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+    public static void main(String[] args) {
+        System.out.println("user");
+    }
+
+}
